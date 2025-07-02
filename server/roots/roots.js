2cd42bbaac215b412fe0aa18e582a7d21b050542
ashu-choudhury <=>
2025-07-03 01:12:22 +0530

    remove server

--- server/roots/roots.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const os = require("node:os");
-
-const router = express.Router();
-router.get("/", (req, res) => {
-    return res.json({msg: "hello!", user: os.uptime()});
-})
-module.exports = router;
